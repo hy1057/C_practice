ad208d41d72ad3a03e6597fdf9d9d952d70da185
hy1057 <66416391+hy1057@users.noreply.github.com>
2020-09-18 14:25:30 +0800

    Update 20200918.C

--- 20200918.C
+++ 20200918.C
@@ -1,10 +1,18 @@
+//华为笔试
+//倒车入库问题
+//输入为车辆经过的区域编号，0~10
+//若按照标准路线行驶，输出“success”
+//若出现字母，表示撞到杆子，输出“zhuang gan”
+//若出现-1，表示超出停车范围，输出“chu jie”
+//若不按顺序经过区域，输出“bad path”
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
 
 int main() {
     char sss[100];
-    scanf("%s",&sss);//继续输入数字。
+    scanf("%s",&sss);
     int len;
     //check if success
     char stdData[] = "0,1,2,3,2,1,0";
